8ceddd2a71df557a2159ef963cd7845baa0fe48f
mbigorne <mathieu.bigorne@gmail.com>
2011-10-18 20:34:45 +0200

    retours en JSON des todos

--- src/main/java/fr/poc/spring/HomeController.java
+++ src/main/java/fr/poc/spring/HomeController.java
@@ -1,5 +1,6 @@
 package fr.poc.spring;
 
+import java.util.List;
 import java.util.Locale;
 
 import javax.inject.Inject;
@@ -10,21 +11,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+
 	@Inject
 	private TodoService todoService;
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {		
-		todoService.createTodo("New item");	
-		model.addAttribute("todos", todoService.getTodos());
-		
-		return "home";
+	public ModelAndView home(Locale locale, Model model) {
+		todoService.createTodo("New item");
+        ModelAndView mav = new ModelAndView();
+
+		mav.addObject("todos", todoService.getTodos());
+		mav.setViewName("home");
+		return mav;
 	}
-	
+
+	@RequestMapping(value = "/json", method = RequestMethod.GET)
+	public @ResponseBody List<Todo> getTodoInJSON() {
+		return todoService.getTodos();
+	}
+
 }
